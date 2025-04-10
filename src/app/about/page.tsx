4d2186d9293dc20bc902bc7a3193280998a266f2
Khalid Alkhalili <kfkhalili@hotmail.com>
2025-04-10 21:37:11 +0200

    fix: correct email

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -101,11 +101,11 @@ export default function About() {
               Follow on LinkedIn
             </SocialLink>
             <SocialLink
-              href="mailto:contact@hudaabukhoti.com"
+              href="mailto:hudaabukhoti@gmail.com"
               icon={MailIcon}
               className="mt-8 border-t border-zinc-100 pt-8 dark:border-zinc-700/40"
             >
-              contact@hudaabukhoti.com
+              hudaabukhoti@gmail.com
             </SocialLink>
           </ul>
         </div>
