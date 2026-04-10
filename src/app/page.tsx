ab7e6431863ed9233c535a79e9bbba0bb90890a3
Huda Abukhoti <hudaabukhoti@gmail.com>
2026-04-10 20:08:43 +0200

    Update homepage title to Artist, Designer, and Storyteller

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -179,7 +179,7 @@ export default async function Home() {
       <Container className="mt-9">
         <div className="max-w-2xl">
           <h1 className="text-4xl font-bold tracking-tight text-zinc-800 sm:text-5xl dark:text-zinc-100">
-            Artist, Designer, and Entrepreneur.
+            Artist, Designer, and Storyteller.
           </h1>
           <p className="mt-6 text-base text-zinc-600 dark:text-zinc-400">
             Hello! I’m Huda, a Munich-based Digital Marketing Consultant with a
