5bad6619fb7e322291d17c33af21f23443912c84
Huda Abukhoti <hudaabukhoti@gmail.com>
2026-04-10 20:04:58 +0200

    Update homepage photo rows and add ten unique photo grid assets

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,17 +2,21 @@ import Image, { type ImageProps } from 'next/image'
 import Link from 'next/link'
 import clsx from 'clsx'
 
-import { Button } from '@/components/Button'
 import { Container } from '@/components/Container'
 import { InstagramIcon, LinkedInIcon } from '@/components/SocialIcons'
 import logoSharjahLadies from '@/images/logos/sharjah-ladies.png'
 import logoAlGhurair from '@/images/logos/al-ghurair-construction.png'
 import logoTrendOnSocial from '@/images/logos/trend-on-social.png'
-import image1 from '@/images/photos/image-1.jpg'
-import image2 from '@/images/photos/image-2.jpg'
-import image3 from '@/images/photos/image-3.jpg'
-import image4 from '@/images/photos/image-4.jpg'
-import image5 from '@/images/photos/image-5.jpg'
+import image1 from '@/images/photos/photo-grid-1.png'
+import image2 from '@/images/photos/photo-grid-2.jpg'
+import image3 from '@/images/photos/photo-grid-3.png'
+import image4 from '@/images/photos/photo-grid-4.jpg'
+import image5 from '@/images/photos/photo-grid-5.jpg'
+import image6 from '@/images/photos/photo-grid-6.png'
+import image7 from '@/images/photos/photo-grid-7.png'
+import image8 from '@/images/photos/photo-grid-8.png'
+import image9 from '@/images/photos/photo-grid-9.jpg'
+import image10 from '@/images/photos/photo-grid-10.jpg'
 
 function BriefcaseIcon(props: React.ComponentPropsWithoutRef<'svg'>) {
   return (
@@ -32,19 +36,6 @@ function BriefcaseIcon(props: React.ComponentPropsWithoutRef<'svg'>) {
       <path
         d="M3 14.25h6.249c.484 0 .952-.002 1.316.319l.777.682a.996.996 0 0 0 1.316 0l.777-.682c.364-.32.832-.319 1.316-.319H21M8.75 6.5V4.75a2 2 0 0 1 2-2h2.5a2 2 0 0 1 2 2V6.5"
         className="stroke-zinc-400 dark:stroke-zinc-500"
-      />
-    </svg>
-  )
-}
-
-function ArrowDownIcon(props: React.ComponentPropsWithoutRef<'svg'>) {
-  return (
-    <svg viewBox="0 0 16 16" fill="none" aria-hidden="true" {...props}>
-      <path
-        d="M4.75 8.75 8 12.25m0 0 3.25-3.5M8 12.25v-8.5"
-        strokeWidth="1.5"
-        strokeLinecap="round"
-        strokeLinejoin="round"
       />
     </svg>
   )
@@ -151,17 +142,21 @@ function Resume() {
   )
 }
 
-function Photos() {
+function Photos({
+  images,
+}: {
+  images: ImageProps['src'][]
+}) {
   let rotations = ['rotate-2', '-rotate-2', 'rotate-2', 'rotate-2', '-rotate-2']
 
   return (
     <div className="mt-16 sm:mt-20">
       <div className="-my-4 flex justify-center gap-5 overflow-hidden py-4 sm:gap-8">
-        {[image1, image2, image3, image4, image5].map((image, imageIndex) => (
+        {images.map((image, imageIndex) => (
           <div
-            key={image.src}
+            key={imageIndex}
             className={clsx(
-              'relative aspect-15/16 w-44 flex-none overflow-hidden rounded-xl bg-zinc-100 sm:w-72 sm:rounded-2xl dark:bg-zinc-800',
+              'relative aspect-[4/5] w-44 flex-none overflow-hidden rounded-xl bg-zinc-100 sm:w-72 sm:rounded-2xl dark:bg-zinc-800',
               rotations[imageIndex % rotations.length],
             )}
           >
@@ -206,14 +201,8 @@ export default async function Home() {
           </div>
         </div>
       </Container>
-      <Photos />
-      <Container className="mt-24 md:mt-28">
-        <div className="mx-auto grid max-w-xl grid-cols-1 gap-y-20 lg:max-w-none lg:grid-cols-2">
-          <div className="space-y-10 lg:pl-16 xl:pl-24">
-            <Resume />
-          </div>
-        </div>
-      </Container>
+      <Photos images={[image1, image2, image3, image4, image5]} />
+      <Photos images={[image6, image7, image8, image9, image10]} />
     </>
   )
 }
